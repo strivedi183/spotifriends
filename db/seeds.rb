0670d419e01e40746f722989bd7302f51e26f63a
Tony Contreras <tony.contreras@gmail.com>
2013-03-12 01:41:30 -0400

    adding results page at 1:40am. what could go wrong?

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,21 @@ u1 = User.create(:first_name => 'Tony', :last_name => 'Contreras', :email => 'to
 u2 = User.create(:first_name => 'Aly', :last_name => 'Contreras', :email => 'alyfc33@gmail.com', :password => 'abc', :password_confirmation => 'abc' )
 u3 = User.create(:first_name => 'T', :last_name => 'Poo', :email => 'tony_poo@hotmail.com', :password => 'abc', :password_confirmation => 'abc' )
 u4 = User.create(:first_name => 'Ed', :last_name => 'M', :email => 'em@ln.com', :password => 'abc', :password_confirmation => 'abc' )
+u5 = User.create(:first_name => 'A', :last_name => 'M', :email => 'a', :password => 'abc', :password_confirmation => 'abc' )
 
 g1 = Group.create(:name => '310 Rock Starz', :description => 'We will rock you')
 g2 = Group.create(:name => 'LN alumns', :description => 'Wheres papis money?')
 
-c1 = Contest.create(:name => 'Death to pop', :description => 'Word. It sucks', :start_date => '2012-03-07', :start_time => '12:00', :end_date => '2012-03-10', :end_time => '20:00')
+c1 = Contest.create(:name => 'Greatest city song', :description => 'There have been many great songs written about great cities...think New York New York, and... uhm... well, I dont want to give you any more hints. Good luck!', :start_date => '2012-03-07', :start_time => '12:00', :end_date => '2012-03-10', :end_time => '20:00')
 
-s1 = Song.create(:spy_id => '1oHxIPqJyvAYHy0PVrDU98', :description => 'This songs makes me want to dance!')
-s2 = Song.create(:spy_id => '79ek2oolIYyGUO4aVK5IY2', :description => 'Hot track')
-s3 = Song.create(:spy_id => '7cbkZYla8cW6SNNDh1EOop', :description => 'EWWW!')
-s4 = Song.create(:spy_id => '1Tfsi3pKqkmgOuHv9esy0F', :description => 'Ass shaker')
+s1 = Song.create(:spy_id => '1HosEpaLu3kIe0nGHMvfDw', :description => 'This songs makes me want to dance!')
+s2 = Song.create(:spy_id => '542whmjdvS8XGZbH8UM2q9', :description => 'Aww man, this song is so awesome')
+s3 = Song.create(:spy_id => '4bBgZTIRQ2PHonaRwnn3jf', :description => 'EWWW!')
+s4 = Song.create(:spy_id => '5LOPo4Ykl6J74fajSPdady', :description => 'Ass shaker')
+s5 = Song.create(:spy_id => '6LRVvIaw04OABi2HX07imO', :description => 'So good!')
 
 #add 3 users to group 1
-g1.users << u1 << u2 << u3
+g1.users << u1 << u2 << u3 << u4 << u5
 #add 2 users to group 2
 g2.users << u1 << u4
 
@@ -34,9 +36,8 @@ c1.save
 #s1.users << u1
 #c1.songs << s1
 s2.users << u2
-c1.songs << s2
 s3.users << u3
-c1.songs << s3
+s4.users << u4
+s5.users << u5
+c1.songs << s2 << s3 << s4 << s5
 
-#and add 1 more song to user 1 to test
-s4.users << u1
