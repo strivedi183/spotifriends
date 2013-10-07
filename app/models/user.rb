f35fc03f1b2b707fa810c8501ae3228bb47ceb0b
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-10-06 21:49:13 -0400

    fixing group stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,25 @@
 #
 # Table name: users
 #
-#  id              :integer          not null, primary key
-#  first_name      :text
-#  last_name       :text
-#  email           :text
-#  image           :string(255)
-#  password_digest :string(255)
-#  is_admin        :boolean
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
+#  id                     :integer          not null, primary key
+#  first_name             :text
+#  last_name              :text
+#  email                  :text
+#  image                  :string(255)
+#  is_admin               :boolean
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0)
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  authentication_token   :string(255)
+#  logged_in_with_pwd     :boolean
 #
 
 
